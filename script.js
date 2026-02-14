ad99dea6c8fbfaa609b0293c5e9b88907a56dfcd
NNKGV <140228207+NNKGV@users.noreply.github.com>
2026-02-14 13:02:35 +0700

    Refactor openLetter function and add sendMessage function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function openLetter(){
     document.querySelector(".envelope").classList.toggle("open");
 }
+
+function sendMessage(){
+    const input = document.getElementById("inputMsg");
+    const display = document.getElementById("chatDisplay");
+
+    if(input.value.trim() !== ""){
+        const p = document.createElement("p");
+        p.textContent = "💌 " + input.value;
+        display.appendChild(p);
+        input.value = "";
+    }
+}
